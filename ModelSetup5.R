e17798c38afb4a171aa69f74f2278ab7f2a81105
LeCrone <charlie.lecrone@compasspoint-mccann.com>
2016-12-15 06:07:27 -0600

    New Stack

---
+++ ModelSetup5.R
@@ -0,0 +1,66 @@
+### Model House Prices 4
+
+# Load libraries
+library(MASS)
+library(caret)
+library(caretEnsemble)
+library(xgboost)
+library(data.table)
+library(Matrix)
+library(methods)
+library(vcd)
+library(e1071)
+library(DMwR)
+library(mboost)
+library(randomForest)
+library(earth)
+library(elasticnet)
+library(arm)
+library(rpart)
+library(Cubist)
+library(kernlab)
+
+seed<-1234
+
+# Get data
+
+training<-read.csv("C:/Kaggle/HousePrices/train3.csv")
+testing<-read.csv("C:/Kaggle/HousePrices/test3.csv")
+
+PreObj<-preProcess(training[,2:250], method = c("zv", "center", "scale"))
+trntrans<-predict(PreObj, training[, 2:250])
+training<-as.data.frame(cbind(SalePrice=log(training$SalePrice), trntrans))
+testing<-predict(PreObj, testing)
+
+# Stacked model ensemble
+
+# Set up training control
+fitControl<-trainControl(method="repeatedcv", number=10, repeats=3, savePredictions = "final")
+
+# Set up model list
+modellist<-c("gbm", "bagEarthGCV", "bayesglm", "rpart", "cubist", "svmRadialCost", "rvmLinear", "rf")
+
+# run models
+
+## initialize for parallel processing
+
+library(doSNOW)
+getDoParWorkers()
+registerDoSNOW(makeCluster(7, type="SOCK"))
+getDoParWorkers()
+getDoParName()
+library(foreach)
+
+set.seed(seed)
+
+models<-caretList(SalePrice~., data=training, metric="RMSE", trControl = fitControl, methodList = modellist)
+
+results<-resamples(models)
+
+summary(results)
+dotplot(results)
+
+modelCor(results)
+splom(results)
+
+
